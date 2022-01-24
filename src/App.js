cc66de2ce6718b6e3f57c3d6ad6c99bf1890968d
Aymane Daif <aymaned345@gmail.com>
2022-01-24 17:23:56 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import { Flex } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import PasswordGenerator from './components/PasswordGenerator';
 import SideBar from './components/SideBar';
 import SwitchTheme from './components/SwitchTheme';
 
 function App() {
   return (
-    <Flex>
-      <SideBar />
-      <main>
-        <PasswordGenerator />
-      </main>
-      <SwitchTheme />
-    </Flex>
+    <>
+      <div>header</div>
+      <Flex>
+        <SideBar />
+        <main>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route
+                path='/passwordGenerator'
+                element={<PasswordGenerator />}
+              />
+            </Routes>
+          </BrowserRouter>
+        </main>
+        <SwitchTheme />
+      </Flex>
+    </>
   );
 }
 
